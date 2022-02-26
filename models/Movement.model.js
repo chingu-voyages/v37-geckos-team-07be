0f879977d93885e2ca9483b3bfa263f07829211a
Giulio <giulio.toldo@gmail.com>
2022-02-26 10:57:03 +0100

    add movement schema and model

---
+++ models/Movement.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+const ObjectId = Schema.Types.ObjectId;
+
+// TODO: Please edit the user model to whatever makes sense for our project
+const movementSchema = new Schema(
+	{
+		userId: { type: ObjectId, required: true },
+		amount: { type: Decimal128, required: true },
+		category: { type: String, required: true },
+		date: { type: Date, required: true },
+		description: { type: String, required: true },
+		isIncome: { type: Boolean, required: true }
+	},
+	{
+		// this second object adds extra properties: `createdAt` and `updatedAt`
+		timestamps: true
+	}
+);
+
+const Movement = model('Movement', movementSchema);
+
+module.exports = Movement;
