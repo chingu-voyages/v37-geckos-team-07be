28b67feb6fca2fd75a79107022134a2f7f031803
Giulio <giulio.toldo@gmail.com>
2022-02-26 12:41:33 +0100

    add /userData and /movementsData routes to get user and movements with user _id

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -12,13 +12,33 @@ router.get('/sampleData', async (req, res, next) => {
 	const user = await User.findOne({
 		_id: ObjectId('62154c056065f32a2bdd0aab')
 	});
+
 	const movements = await Movement.find({
 		userId: ObjectId('62154c056065f32a2bdd0aab')
 	});
+
 	res.json({
 		user,
 		movements
 	});
+});
+
+/* GET User data from user _id */
+router.post('/userData', async (req, res, next) => {
+	const user = await User.findOne({
+		_id: ObjectId(req.body.userId)
+	});
+
+	res.json(user);
+});
+
+/* GET Movement data from user _id */
+router.post('/movementsData', async (req, res, next) => {
+	const movements = await Movement.find({
+		userId: ObjectId(req.body.userId)
+	});
+
+	res.json(movements);
 });
 
 module.exports = router;
